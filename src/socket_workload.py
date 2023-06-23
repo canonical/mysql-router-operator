61ad7e5aad9ce04510a65b001f53604b20840345
Carl Csaposs <carl.csaposs@canonical.com>
2023-06-23 12:53:32 +0000

    type annotation

--- src/socket_workload.py
+++ src/socket_workload.py
@@ -17,7 +17,7 @@ class SocketWorkload(workload.Workload):
 class AuthenticatedSocketWorkload(workload.AuthenticatedWorkload, SocketWorkload):
     """Workload with connection to MySQL cluster and with Unix sockets enabled"""
 
-    def _get_bootstrap_command(self, password: str):
+    def _get_bootstrap_command(self, password: str) -> list[str]:
         command = super()._get_bootstrap_command(password)
         command.extend(
             [
