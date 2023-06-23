42f44290a84e105560808f2a9f48ce2f88f27e17
Carl Csaposs <carl.csaposs@canonical.com>
2023-06-23 12:57:33 +0000

    set workload version

--- src/machine_charm.py
+++ src/machine_charm.py
@@ -45,6 +45,7 @@ class MachineSubordinateRouterCharm(abstract_charm.MySQLRouterCharm):
 
     def _on_install(self, _) -> None:
         snap.Installer().install(unit=self.unit)
+        self.unit.set_workload_version(self.get_workload(event=None).version)
 
     def _on_remove(self, _) -> None:
         snap.Installer().uninstall()
