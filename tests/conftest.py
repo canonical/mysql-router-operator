13eead15c0ea4c19487aded98de1be99e9d4552e
Judit Novak <judit.novak@canonical.com>
2023-10-19 01:35:55 +0200

    Test changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@
 
 import argparse
 
+import pytest
+
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -28,3 +30,20 @@ def pytest_configure(config):
         config.option.mysql_router_charm_series = "jammy"
     if config.option.mysql_router_charm_bases_index is None:
         config.option.mysql_router_charm_bases_index = 1
+
+
+@pytest.fixture
+def only_with_juju_secrets(juju_has_secrets):
+    """Pretty way to skip Juju 3 tests."""
+    if not juju_has_secrets:
+        pytest.skip("Secrets test only applies on Juju 3.x")
+
+
+@pytest.fixture
+def only_without_juju_secrets(juju_has_secrets):
+    """Pretty way to skip Juju 2-specific tests.
+
+    Typically: to save CI time, when the same check were executed in a Juju 3-specific way already
+    """
+    if juju_has_secrets:
+        pytest.skip("Skipping legacy secrets tests")
--- tests/unit/scenario_/database_relations/test_database_relations.py
+++ tests/unit/scenario_/database_relations/test_database_relations.py
@@ -14,7 +14,9 @@ import machine_charm
 from . import combinations
 
 
-def output_states(*, relations: list[scenario.Relation]) -> typing.Iterable[scenario.State]:
+def output_states(
+    *, relations: list[scenario.Relation], secrets: list[scenario.Secret] = []
+) -> typing.Iterable[scenario.State]:
     """Run scenario test for each `abstract_charm.reconcile_database_relations` event.
 
     Excludes *-relation-breaking events
@@ -22,8 +24,8 @@ def output_states(*, relations: list[scenario.Relation]) -> typing.Iterable[scen
     The output state of each test should be identical for all events.
     """
     context = scenario.Context(machine_charm.MachineSubordinateRouterCharm)
-    input_state = scenario.State(
         relations=[*relations, scenario.PeerRelation(endpoint="upgrade-version-a")],
+        secrets=secrets,
         leader=True,
     )
     events = []
@@ -54,11 +56,22 @@ def test_missing_requires():
         assert state.app_status == ops.BlockedStatus("Missing relation: backend-database")
 
 
+@pytest.mark.usefixtures("only_without_juju_secrets")
 def test_missing_provides(incomplete_requires):
     for state in output_states(relations=[incomplete_requires]):
         assert state.app_status == ops.BlockedStatus("Missing relation: database")
 
 
+@pytest.mark.usefixtures("only_with_juju_secrets")
+@pytest.mark.parametrize(
+    "incomplete_requires_s, secret", combinations.incomplete_requires_secret(1)
+)
+def test_missing_provides_secrets(incomplete_requires_s, secret):
+    for state in output_states(relations=[incomplete_requires_s], secrets=[secret]):
+        assert state.app_status == ops.BlockedStatus("Missing relation: database")
+
+
+@pytest.mark.usefixtures("only_without_juju_secrets")
 @pytest.mark.parametrize(
     "unsupported_extra_user_role_provides_s",
     combinations.unsupported_extra_user_role_provides(1, 3),
@@ -75,10 +88,38 @@ def test_provides_unsupported_extra_user_role(
         ]
     ):
         assert state.app_status == ops.BlockedStatus(
-            f"{unsupported_extra_user_role_provides_s[0].remote_app_name} app requested unsupported extra user role on database endpoint"
+            f"{unsupported_extra_user_role_provides_s[0].remote_app_name} app "
+            "requested unsupported extra user role on database endpoint"
         )
 
 
+@pytest.mark.usefixtures("only_with_juju_secrets")
+@pytest.mark.parametrize(
+    "unsupported_extra_user_role_provides_s",
+    combinations.unsupported_extra_user_role_provides(1, 3),
+)
+@pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(0, 2))
+@pytest.mark.parametrize(
+    "incomplete_requires_s, secret", combinations.incomplete_requires_secret(1)
+)
+def test_provides_unsupported_extra_user_role_secrets(
+    incomplete_requires_s, secret, complete_provides_s, unsupported_extra_user_role_provides_s
+):
+    for state in output_states(
+        relations=[
+            incomplete_requires_s,
+            *complete_provides_s,
+            *unsupported_extra_user_role_provides_s,
+        ],
+        secrets=[secret],
+    ):
+        assert state.app_status == ops.BlockedStatus(
+            f"{unsupported_extra_user_role_provides_s[0].remote_app_name} app "
+            "requested unsupported extra user role on database endpoint"
+        )
+
+
+@pytest.mark.usefixtures("only_without_juju_secrets")
 @pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(1, 2))
 def test_incomplete_requires(incomplete_requires, complete_provides_s):
     for state in output_states(relations=[incomplete_requires, *complete_provides_s]):
@@ -89,6 +130,23 @@ def test_incomplete_requires(incomplete_requires, complete_provides_s):
             assert state.relations[index].local_app_data == {}
 
 
+@pytest.mark.usefixtures("only_with_juju_secrets")
+@pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(1, 2))
+@pytest.mark.parametrize(
+    "incomplete_requires_s, secret", combinations.incomplete_requires_secret(1)
+)
+def test_incomplete_requires_secrets(incomplete_requires_s, secret, complete_provides_s):
+    for state in output_states(
+        relations=[incomplete_requires_s, *complete_provides_s], secrets=[secret]
+    ):
+        assert state.app_status == ops.WaitingStatus(
+            f"Waiting for {incomplete_requires_s.remote_app_name} app on backend-database endpoint"
+        )
+        for index, provides in enumerate(complete_provides_s, 1):
+            assert state.relations[index].local_app_data == {}
+
+
+@pytest.mark.usefixtures("only_without_juju_secrets")
 @pytest.mark.parametrize(
     "unsupported_extra_user_role_provides_s",
     combinations.unsupported_extra_user_role_provides(1, 3),
@@ -107,7 +165,8 @@ def test_complete_requires_and_provides_unsupported_extra_user_role(
         ]
     ):
         assert state.app_status == ops.BlockedStatus(
-            f"{unsupported_extra_user_role_provides_s[0].remote_app_name} app requested unsupported extra user role on database endpoint"
+            f"{unsupported_extra_user_role_provides_s[0].remote_app_name} app "
+            "requested unsupported extra user role on database endpoint"
         )
         for index, provides in enumerate(complete_provides_s, 1):
             local_app_data = state.relations[index].local_app_data
@@ -124,6 +183,47 @@ def test_complete_requires_and_provides_unsupported_extra_user_role(
             assert state.relations[index].local_app_data == {}
 
 
+@pytest.mark.usefixtures("only_with_juju_secrets")
+@pytest.mark.parametrize(
+    "unsupported_extra_user_role_provides_s",
+    combinations.unsupported_extra_user_role_provides(1, 3),
+)
+@pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(0, 2))
+@pytest.mark.parametrize("complete_requires_s, secret", combinations.complete_requires_secret(1))
+def test_complete_requires_and_provides_unsupported_extra_user_role_secret(
+    complete_requires_s,
+    secret,
+    complete_provides_s,
+    unsupported_extra_user_role_provides_s,
+):
+    for state in output_states(
+        relations=[
+            complete_requires_s,
+            *complete_provides_s,
+            *unsupported_extra_user_role_provides_s,
+        ],
+        secrets=[secret],
+    ):
+        assert state.app_status == ops.BlockedStatus(
+            f"{unsupported_extra_user_role_provides_s[0].remote_app_name} app "
+            "requested unsupported extra user role on database endpoint"
+        )
+        for index, provides in enumerate(complete_provides_s, 1):
+            local_app_data = state.relations[index].local_app_data
+            assert len(local_app_data.pop("password")) > 0
+            assert local_app_data == {
+                "database": provides.remote_app_data["database"],
+                "endpoints": "file:///var/snap/charmed-mysql/common/run/mysqlrouter/mysql.sock",
+                "read-only-endpoints": "file:///var/snap/charmed-mysql/common/run/mysqlrouter/mysqlro.sock",
+                "username": f'{secret.contents[0]["username"]}-{provides.relation_id}',
+            }
+        for index, provides in enumerate(
+            unsupported_extra_user_role_provides_s, 1 + len(complete_provides_s)
+        ):
+            assert state.relations[index].local_app_data == {}
+
+
+@pytest.mark.usefixtures("only_without_juju_secrets")
 @pytest.mark.parametrize("incomplete_provides_s", combinations.incomplete_provides(1, 3))
 def test_incomplete_provides(complete_requires, incomplete_provides_s):
     for state in output_states(relations=[complete_requires, *incomplete_provides_s]):
@@ -134,6 +234,21 @@ def test_incomplete_provides(complete_requires, incomplete_provides_s):
             assert state.relations[index].local_app_data == {}
 
 
+@pytest.mark.usefixtures("only_with_juju_secrets")
+@pytest.mark.parametrize("incomplete_provides_s", combinations.incomplete_provides(1, 3))
+@pytest.mark.parametrize("complete_requires_s, secret", combinations.complete_requires_secret(1))
+def test_incomplete_provides_secrets(complete_requires_s, secret, incomplete_provides_s):
+    for state in output_states(
+        relations=[complete_requires_s, *incomplete_provides_s], secrets=[secret]
+    ):
+        assert state.app_status == ops.WaitingStatus(
+            f"Waiting for {incomplete_provides_s[0].remote_app_name} app on database endpoint"
+        )
+        for index, provides in enumerate(incomplete_provides_s, 1):
+            assert state.relations[index].local_app_data == {}
+
+
+@pytest.mark.usefixtures("only_without_juju_secrets")
 @pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(1, 2, 4))
 def test_complete_provides(complete_requires, complete_provides_s):
     for state in output_states(relations=[complete_requires, *complete_provides_s]):
@@ -149,8 +264,28 @@ def test_complete_provides(complete_requires, complete_provides_s):
             }
 
 
+@pytest.mark.usefixtures("only_with_juju_secrets")
+@pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(1, 2, 4))
+@pytest.mark.parametrize("complete_requires_s, secret", combinations.complete_requires_secret(1))
+def test_complete_provides_secret(complete_requires_s, secret, complete_provides_s):
+    for state in output_states(
+        relations=[complete_requires_s, *complete_provides_s], secrets=[secret]
+    ):
+        assert state.app_status == ops.ActiveStatus()
+        for index, provides in enumerate(complete_provides_s, 1):
+            local_app_data = state.relations[index].local_app_data
+            assert len(local_app_data.pop("password")) > 0
+            assert local_app_data == {
+                "database": provides.remote_app_data["database"],
+                "endpoints": "file:///var/snap/charmed-mysql/common/run/mysqlrouter/mysql.sock",
+                "read-only-endpoints": "file:///var/snap/charmed-mysql/common/run/mysqlrouter/mysqlro.sock",
+                "username": f'{secret.contents[0]["username"]}-{provides.relation_id}',
+            }
+
+
+@pytest.mark.usefixtures("only_without_juju_secrets")
 @pytest.mark.parametrize("incomplete_provides_s", combinations.incomplete_provides(1, 3))
-@pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(1, 3))
+@pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(1))
 def test_complete_provides_and_incomplete_provides(
     complete_requires, complete_provides_s, incomplete_provides_s
 ):
@@ -168,6 +303,35 @@ def test_complete_provides_and_incomplete_provides(
                 "endpoints": "file:///var/snap/charmed-mysql/common/run/mysqlrouter/mysql.sock",
                 "read-only-endpoints": "file:///var/snap/charmed-mysql/common/run/mysqlrouter/mysqlro.sock",
                 "username": f'{complete_requires.remote_app_data["username"]}-{provides.relation_id}',
+            }
+        for index, provides in enumerate(incomplete_provides_s, 1 + len(complete_provides_s)):
+            assert state.relations[index].local_app_data == {}
+
+
+@pytest.mark.usefixtures("only_with_juju_secrets")
+@pytest.mark.parametrize("incomplete_provides_s", combinations.incomplete_provides(1, 3))
+@pytest.mark.parametrize("complete_provides_s", combinations.complete_provides(1))
+@pytest.mark.parametrize(
+    "complete_requires_s, secret", combinations.complete_requires_secret(1, 2, 4)
+)
+def test_complete_provides_and_incomplete_provides_secret(
+    complete_requires_s, secret, complete_provides_s, incomplete_provides_s
+):
+    for state in output_states(
+        relations=[complete_requires_s, *complete_provides_s, *incomplete_provides_s],
+        secrets=[secret],
+    ):
+        assert state.app_status == ops.WaitingStatus(
+            f"Waiting for {incomplete_provides_s[0].remote_app_name} app on database endpoint"
+        )
+        for index, provides in enumerate(complete_provides_s, 1):
+            local_app_data = state.relations[index].local_app_data
+            assert len(local_app_data.pop("password")) > 0
+            assert local_app_data == {
+                "database": provides.remote_app_data["database"],
+                "endpoints": "file:///var/snap/charmed-mysql/common/run/mysqlrouter/mysql.sock",
+                "read-only-endpoints": "file:///var/snap/charmed-mysql/common/run/mysqlrouter/mysqlro.sock",
+                "username": f'{secret.contents[0]["username"]}-{provides.relation_id}',
             }
         for index, provides in enumerate(incomplete_provides_s, 1 + len(complete_provides_s)):
             assert state.relations[index].local_app_data == {}
