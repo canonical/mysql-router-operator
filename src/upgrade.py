3570ef5c7190578466827f812639704f0f09e320
Carl Csaposs <carl.csaposs@canonical.com>
2023-10-23 08:13:14 +0000

    Sync files with K8s

--- src/upgrade.py
+++ src/upgrade.py
@@ -215,7 +215,7 @@ class Upgrade(abc.ABC):
         - confirm first upgraded unit is healthy and resume upgrade
         - force upgrade of next unit if 1 or more upgraded units are unhealthy
         """
-        force = action_event and action_event.params.get("force") is True
+        force = bool(action_event and action_event.params.get("force") is True)
 
         units = self._sorted_units
 
@@ -226,7 +226,7 @@ class Upgrade(abc.ABC):
             for upgrade_order_index, unit in enumerate(units):
                 # Note: upgrade_order_index != unit number
                 if (
-                    force is False and self._peer_relation.data[unit].get("state") != "healthy"
+                    not force and self._peer_relation.data[unit].get("state") != "healthy"
                 ) or self._unit_workload_versions[unit.name] != self._app_workload_version:
                     if not action_event and upgrade_order_index == 1:
                         # User confirmation needed to resume upgrade (i.e. upgrade second unit)
