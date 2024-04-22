22344cc7f30debec88b38af47fcecc64251b51c6
Carl Csaposs <carl.csaposs@canonical.com>
2024-04-22 13:00:44 +0200

    Sync upgrade status with K8s charm
    Add charm version to status
    
    https://github.com/canonical/mysql-router-k8s-operator/pull/230

--- src/machine_upgrade.py
+++ src/machine_upgrade.py
@@ -48,20 +48,12 @@ class Upgrade(upgrade.Upgrade):
     def _get_unit_healthy_status(
         self, *, workload_status: typing.Optional[ops.StatusBase]
     ) -> ops.StatusBase:
-        if self._unit_workload_container_version == self._app_workload_container_version:
-            if isinstance(workload_status, ops.WaitingStatus):
-                return ops.WaitingStatus(
-                    f"Router {self._unit_workload_version} rev {self._unit_workload_container_version}"
-                )
-            return ops.ActiveStatus(
-                f"Router {self._unit_workload_version} rev {self._unit_workload_container_version} running"
-            )
         if isinstance(workload_status, ops.WaitingStatus):
             return ops.WaitingStatus(
-                f"Charmed operator upgraded. Router {self._unit_workload_version} rev {self._unit_workload_container_version}"
+                f'Router {self._unit_workload_version}. Snap rev {self._unit_workload_container_version}. Charmed operator {self._current_versions["charm"]}'
             )
-        return ops.WaitingStatus(
-            f"Charmed operator upgraded. Router {self._unit_workload_version} rev {self._unit_workload_container_version} running"
+        return ops.ActiveStatus(
+            f'Router {self._unit_workload_version} running. Snap rev {self._unit_workload_container_version}. Charmed operator {self._current_versions["charm"]}'
         )
 
     @property
