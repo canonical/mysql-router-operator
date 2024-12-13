9b64f258baa9b08ed01ee43799f5fca2ae77d4c2
Sinclert Pérez <sinclert.perez@canonical.com>
2024-12-13 14:29:26 +0100

    Replace unittest patch by monkeypath

--- tests/unit/test_architecture.py
+++ tests/unit/test_architecture.py
@@ -2,8 +2,6 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-from unittest.mock import patch
-
 from architecture import is_wrong_architecture
 
 TEST_MANIFEST = """
@@ -15,38 +13,36 @@ TEST_MANIFEST = """
 """
 
 
-def test_wrong_architecture_file_not_found():
+def test_wrong_architecture_file_not_found(monkeypatch):
     """Tests if the function returns False when the charm file doesn't exist."""
-    with (
-        patch("os.environ", return_value={"CHARM_DIR": "/tmp"}),
-        patch("pathlib.Path.exists", return_value=False),
-    ):
-        assert not is_wrong_architecture()
+    monkeypatch.setattr("os.environ", {"CHARM_DIR": "/tmp"})
+    monkeypatch.setattr("pathlib.Path.exists", lambda *args, **kwargs: False)
+    assert not is_wrong_architecture()
 
 
-def test_wrong_architecture_amd64():
+def test_wrong_architecture_amd64(monkeypatch):
     """Tests if the function correctly identifies arch when charm is AMD."""
-    with (
-        patch("os.environ", return_value={"CHARM_DIR": "/tmp"}),
-        patch("pathlib.Path.exists", return_value=True),
-        patch("pathlib.Path.read_text", return_value=TEST_MANIFEST.format(arch="amd64")),
-        patch("platform.machine") as machine,
-    ):
-        machine.return_value = "x86_64"
-        assert not is_wrong_architecture()
-        machine.return_value = "aarch64"
-        assert is_wrong_architecture()
+    manifest = TEST_MANIFEST.format(arch="amd64")
+    monkeypatch.setattr("os.environ", {"CHARM_DIR": "/tmp"})
+    monkeypatch.setattr("pathlib.Path.exists", lambda *args, **kwargs: True)
+    monkeypatch.setattr("pathlib.Path.read_text", lambda *args, **kwargs: manifest)
+
+    monkeypatch.setattr("platform.machine", lambda *args, **kwargs: "x86_64")
+    assert not is_wrong_architecture()
+
+    monkeypatch.setattr("platform.machine", lambda *args, **kwargs: "aarch64")
+    assert is_wrong_architecture()
 
 
-def test_wrong_architecture_arm64():
+def test_wrong_architecture_arm64(monkeypatch):
     """Tests if the function correctly identifies arch when charm is ARM."""
-    with (
-        patch("os.environ", return_value={"CHARM_DIR": "/tmp"}),
-        patch("pathlib.Path.exists", return_value=True),
-        patch("pathlib.Path.read_text", return_value=TEST_MANIFEST.format(arch="arm64")),
-        patch("platform.machine") as machine,
-    ):
-        machine.return_value = "x86_64"
-        assert is_wrong_architecture()
-        machine.return_value = "aarch64"
-        assert not is_wrong_architecture()
+    manifest = TEST_MANIFEST.format(arch="arm64")
+    monkeypatch.setattr("os.environ", {"CHARM_DIR": "/tmp"})
+    monkeypatch.setattr("pathlib.Path.exists", lambda *args, **kwargs: True)
+    monkeypatch.setattr("pathlib.Path.read_text", lambda *args, **kwargs: manifest)
+
+    monkeypatch.setattr("platform.machine", lambda *args, **kwargs: "x86_64")
+    assert is_wrong_architecture()
+
+    monkeypatch.setattr("platform.machine", lambda *args, **kwargs: "aarch64")
+    assert not is_wrong_architecture()
