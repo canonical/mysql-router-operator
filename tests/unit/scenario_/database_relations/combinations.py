fa1879418bfc4c5b45d2ceecd040c87afbb57446
Carl Csaposs <carl.csaposs@canonical.com>
2025-02-04 09:24:45 +0100

    Fix unit tests

--- tests/unit/scenario_/database_relations/combinations.py
+++ tests/unit/scenario_/database_relations/combinations.py
@@ -5,20 +5,11 @@
 
 import itertools
 import typing
-from uuid import uuid4
 
 import scenario
 
-APP_NAMES = ["remote", "mysql-k8s"]
-SECRET_USER = scenario.Secret(
-    id="myXID",  # Must be defined for obj instantiation, but we override it later
-    owner="mysql-router",
-    contents={0: {"username": "relation-68", "password": "Dy0k2UTfyNt2B13cfe412K7YGs07S4U7"}},
-    remote_grants="myappB",
-)
 
-
-def _relation_provides_combinations(
+def _relation_combinations(
     *, relation_amounts: typing.Iterable[int], relations: list[scenario.SubordinateRelation]
 ) -> list[list[scenario.SubordinateRelation]]:
     """Get all combinations of `relations` for each length in `relation_amounts`."""
@@ -33,49 +24,12 @@ def _relation_provides_combinations(
     return combinations
 
 
-def _relation_requires_combinations(
-    *,
-    relation_amounts: typing.Iterable[int],
-    relations: list[scenario.Relation],
-    secrets: list[scenario.Secret],
-) -> list[list[scenario.Relation]]:
-    """Get all combinations of `relations` for each length in `relation_amounts`."""
-    combinations = []
-    for number_of_relations in relation_amounts:
-        for combination in itertools.combinations_with_replacement(relations, number_of_relations):
-            combination: tuple[scenario.Relation]
-
-            for relation in combination:
-                secret_id = uuid4().hex
-                relation_id = scenario.state.next_relation_id()
-
-                secrets_new = [
-                    secret.replace(
-                        id=secret_id, label=f"{relation.endpoint}.{relation_id}.user.secret"
-                    )
-                    for secret in secrets
-                ]
-
-                remote_app_data_new = relation.remote_app_data
-                remote_app_data_new["secret-user"] = secret_id
-                combinations += [
-                    (
-                        relation.replace(
-                            relation_id=relation_id, remote_app_data=remote_app_data_new
-                        ),
-                        secret_new,
-                    )
-                    for secret_new in secrets_new
-                ]
-    return combinations
-
-
 def incomplete_provides(*relation_amounts: int) -> list[list[scenario.SubordinateRelation]]:
     databags = [{}]
     relations = []
     for remote_app_name in ["remote", "mysql-test-app"]:
         relations.extend(
-            _relation_provides_combinations(
+            _relation_combinations(
                 relation_amounts=relation_amounts,
                 relations=[
                     scenario.SubordinateRelation(
@@ -96,8 +50,18 @@ def unsupported_extra_user_role_provides(
     databags = [
         {"database": "myappA", "extra-user-roles": "admin"},
         {"database": "myappB", "extra-user-roles": "mysqlrouter"},
+        {
+            "database": "myappA",
+            "extra-user-roles": "admin",
+            "requested-secrets": '["username", "password", "tls", "tls-ca", "uris"]',
+        },
+        {
+            "database": "myappB",
+            "extra-user-roles": "mysqlrouter",
+            "requested-secrets": '["username", "password", "tls", "tls-ca", "uris"]',
+        },
     ]
-    return _relation_provides_combinations(
+    return _relation_combinations(
         relation_amounts=relation_amounts,
         relations=[
             scenario.SubordinateRelation(endpoint="database", remote_app_data=databag)
@@ -107,72 +71,22 @@ def unsupported_extra_user_role_provides(
 
 
 def complete_provides(*relation_amounts: int) -> list[list[scenario.SubordinateRelation]]:
-    databags = [{"database": "myappA"}, {"database": "foo"}]
-    return _relation_provides_combinations(
+    databags = [
+        {"database": "myappA"},
+        {"database": "foo"},
+        {
+            "database": "myappA",
+            "requested-secrets": '["username", "password", "tls", "tls-ca", "uris"]',
+        },
+        {
+            "database": "foo",
+            "requested-secrets": '["username", "password", "tls", "tls-ca", "uris"]',
+        },
+    ]
+    return _relation_combinations(
         relation_amounts=relation_amounts,
         relations=[
             scenario.SubordinateRelation(endpoint="database", remote_app_data=databag)
             for databag in databags
         ],
     )
-
-
-def complete_requires_secret(*relation_amounts: int) -> list[list[scenario.Relation]]:
-    relation = scenario.Relation(
-        endpoint="backend-database",
-        remote_app_data={
-            "endpoints": "mysql-k8s-primary:5432"
-        },  # Will be extended with "secret-user" field
-    )
-    return _relation_requires_combinations(
-        relation_amounts=relation_amounts, relations=[relation], secrets=[SECRET_USER]
-    )
-
-
-def incomplete_requires_secret(*relation_amounts: int) -> list[list[scenario.Relation]]:
-    params = [
-        {},
-        {
-            "database": "myappB",
-            "read-only-endpoints": "mysql-k8s-replicas:5432",
-        },
-        {
-            "database": "myappB",
-            "endpoints": "mysql-k8s-primary:5432",
-            "read-only-endpoints": "mysql-k8s-replicas:5432",
-        },
-    ]
-
-    # Missing fields in the databag
-    relations = [
-        scenario.Relation(
-            endpoint="backend-database",
-            remote_app_name=app_name,
-            remote_app_data=param,  # Will be extended with "secret-user" field
-        )
-        for app_name in APP_NAMES
-        for param in params[:2]
-    ]
-    combinations_full_secret = _relation_requires_combinations(
-        relation_amounts=relation_amounts, relations=relations, secrets=[SECRET_USER]
-    )
-
-    # Missing fields in the secret
-    secret_user_pw_missing = SECRET_USER.replace(contents={0: {"username": "relation-68"}})
-
-    relations_broken_secret = [
-        scenario.Relation(
-            endpoint="backend-database",
-            remote_app_name=app_name,
-            remote_app_data=params[2],  # Will be extended with "secret-user" field
-        )
-        for app_name in APP_NAMES
-    ]
-
-    combinations_broken_secret = _relation_requires_combinations(
-        relation_amounts=relation_amounts,
-        relations=relations_broken_secret,
-        secrets=[secret_user_pw_missing],
-    )
-
-    return combinations_full_secret + combinations_broken_secret
