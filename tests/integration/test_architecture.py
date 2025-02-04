bd604ffc98adf52cae6e2645271e5d3f1b99ae8d
Carl Csaposs <carl.csaposs@canonical.com>
2025-02-04 14:33:14 +0100

    fix test_architecture.py

--- tests/integration/test_architecture.py
+++ tests/integration/test_architecture.py
@@ -4,6 +4,7 @@
 
 import asyncio
 
+import pytest
 from pytest_operator.plugin import OpsTest
 
 from . import markers
@@ -13,8 +14,10 @@ MYSQL_TEST_APP_NAME = "mysql-test-app"
 
 
 @markers.amd64_only
-async def test_arm_charm_on_amd_host(ops_test: OpsTest, charm, series) -> None:
+async def test_arm_charm_on_amd_host(ops_test: OpsTest, charm, ubuntu_base, series) -> None:
     """Tries deploying an arm64 charm on amd64 host."""
+    if ubuntu_base == "20.04":
+        pytest.skip("arm64 charm not built for 20.04")
     charm = charm.replace("amd64", "arm64")
 
     await asyncio.gather(
