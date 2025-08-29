73f9c8e1919612a21a8f8c3323760cf031c4aacc
Carl Csaposs <carl.csaposs@canonical.com>
2025-08-29 11:00:40 +0200

    Fix logrotate.py abstraction

--- src/logrotate.py
+++ src/logrotate.py
@@ -7,8 +7,14 @@ https://manpages.ubuntu.com/manpages/jammy/man8/logrotate.8.html
 """
 
 import abc
+import logging
+import pathlib
+
+import jinja2
 
 import container
+
+logger = logging.getLogger(__name__)
 
 
 class LogRotate(abc.ABC):
@@ -17,10 +23,28 @@ class LogRotate(abc.ABC):
     def __init__(self, *, container_: container.Container):
         self._container = container_
 
+        self._logrotate_config = self._container.path("/etc/logrotate.d/flush_mysqlrouter_logs")
+
+    @property
     @abc.abstractmethod
+    def _SYSTEM_USER(self) -> str:  # noqa
+        """The system user that mysqlrouter runs as."""
+
     def enable(self) -> None:
         """Enable logrotate."""
+        logger.debug("Creating logrotate config file")
 
-    @abc.abstractmethod
+        template = jinja2.Template(pathlib.Path("templates/logrotate.j2").read_text())
+
+        log_file_path = self._container.path("/var/log/mysqlrouter/mysqlrouter.log")
+        rendered = template.render(
+            log_file_path=str(log_file_path),
+            system_user=self._SYSTEM_USER,
+        )
+        self._logrotate_config.write_text(rendered)
+
+        logger.debug("Created logrotate config file")
+
     def disable(self) -> None:
         """Disable logrotate."""
+        self._logrotate_config.unlink(missing_ok=True)
